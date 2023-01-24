1f5ff100c9df99166ec65acda46414612dc9da75
Gleb Kushedow <kushedow@gmail.com>
2023-01-24 11:55:58 +0400

    docs and second function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,15 @@
 def str_func(word):
+    """
+    Какая-то документация
+    :param word:
+    :return:
+    """
     return word.upper()
+
+def str_title(word):
+    """
+    Какая-то документация
+    :param word:
+    :return:
+    """
+    return word.title()
